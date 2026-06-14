fa956ecc533643e14c69fdd6405a3588d11cb1cd
Ethan Joseph <ethancredo.ascc@outlook.com>
2026-06-15 07:47:49 +0800

    create node and tree class

---
+++ bst.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor(data) {
+        this.data
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+}
