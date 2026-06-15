b5cb5fb60d5e1bee802349f78dd95f399b889eb1
Ethan <ethancredo.ascc@outlook.com>
2026-06-15 15:40:59 +0800

    create buildTree that accepts array

--- bst.js
+++ bst.js
@@ -1,13 +1,28 @@
 class Node {
     constructor(data) {
-        this.data
+        this.data = data;
         this.left = null;
         this.right = null;
     }
 }
 
-class Tree {
-    constructor() {
+export class Tree {
+    constructor(arr) {
         this.root = null;
+        this.arr = arr;
     }
+
+    buildTree(arr, start = 0, end = arr.length) {
+        if (start > end) return null;
+
+        let mid = start + Math.floor((end - start) / 2);
+        let root = new Node(arr[mid]);
+
+        // divide from middle element
+        root.left = this.buildTree(arr, start, mid - 1);
+        root.right = this.buildTree(arr, mid + 1, end);
+
+        return root;
+    }
+
 }
